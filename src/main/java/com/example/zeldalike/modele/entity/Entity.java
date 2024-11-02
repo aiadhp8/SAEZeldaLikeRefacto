b6c79f550fa74e7e61c798621e44c2fda19a5cb8
AiAdhP8 <aadahchour@iut.univ-paris8.fr>
2024-11-02 18:55:27 +0100

    modif Entity

--- src/main/java/com/example/zeldalike/modele/entity/Entity.java
+++ src/main/java/com/example/zeldalike/modele/entity/Entity.java
@@ -10,4 +10,20 @@ public abstract class Entity {
     public Position getPosition() {
         return position;
     }
+
+    public int getHeight() {
+        return this.position.getHeight();
+    }
+
+    public int getWidth() {
+        return this.position.getWidth();
+    }
+
+    public boolean collision(Entity e){
+        return this.position.collision(e.getPosition());
+    }
+
+    public double distance(Entity e){
+        return this.position.distance(e.getPosition());
+    }
 }
