e8da13a6bc9adee9ca99eeef348db7110b46040a
AiAdhP8 <aadahchour@iut.univ-paris8.fr>
2024-11-17 14:54:01 +0100

    réglage affichage poing et hitbux/hurtbox avec mob

--- src/main/java/com/example/zeldalike/modele/entity/objetImmobile/objetRecuperable/Arme/Arme.java
+++ src/main/java/com/example/zeldalike/modele/entity/objetImmobile/objetRecuperable/Arme/Arme.java
@@ -6,6 +6,7 @@ import com.example.zeldalike.modele.entity.Position;
 import com.example.zeldalike.modele.entity.objetImmobile.objetRecuperable.ObjetRecuperables;
 import com.example.zeldalike.modele.entity.objetMobile.personnage.Personnage;
 import com.example.zeldalike.modele.entity.objetMobile.personnage.ennemi.Ennemis;
+import com.example.zeldalike.modele.entity.objetMobile.personnage.joueur.Joueur;
 
 
 import java.util.ArrayList;
@@ -21,8 +22,9 @@ public abstract class Arme extends ObjetRecuperables {
     }
 
     public void hit(Ennemis e) {
-        if (!e.isBouclierActif())
+        if (!e.isBouclierActif()) {
             e.subirDegats(Math.max(attaque - e.getDef(), 0));
+        }
     }
 
     public abstract void faireUneAttaque();
@@ -33,13 +35,34 @@ public abstract class Arme extends ObjetRecuperables {
         for (Personnage ennemi : ennemisProches) {
             System.out.println(ennemi);
             if (ennemi != null) {
-                if (this.collision(ennemi)) {
+                if (this.inAtkRange(ennemi)) {
                     ennemisToucher.add(ennemi);
                 }
             }
         }
-        System.out.println(ennemisToucher.toString());
         return ennemisToucher;
+    }
+
+    private boolean inAtkRange(Personnage ennemi) {
+        int xTemp = Environnement.getInstance().getJoueur().getPosition().getX();
+        int yTemp = Environnement.getInstance().getJoueur().getPosition().getY();
+        Position positionArmeTemp = new Position(0,0,0,0);
+        switch (Environnement.getInstance().getJoueur().getDirectionPrecedente()){
+            case 1,2,3:
+                positionArmeTemp = new Position(xTemp,yTemp+32,32,32);
+                break;
+            case 4:
+                positionArmeTemp = new Position(xTemp-32,yTemp,32,32);
+
+                break;
+            case 7,8,9:
+                positionArmeTemp = new Position(xTemp,yTemp-32,32,32);
+                break;
+            case 6:
+                positionArmeTemp = new Position(xTemp+32,yTemp,32,32);
+                break;
+        }
+        return positionArmeTemp.collision(ennemi.getPosition());
     }
 
 
